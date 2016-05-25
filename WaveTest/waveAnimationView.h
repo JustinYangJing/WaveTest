c65e408cc445fa44bd87059576e1a374f227e5cf
JustinYang <justinyang@JustinYangdeiMac.local>
2016-05-25 18:33:45 +0800

    new create

---
+++ WaveTest/waveAnimationView.h
@@ -0,0 +1,13 @@
+//
+//  waveAnimationView.h
+//  WaveTest
+//
+//  Created by JustinYang on 16/5/25.
+//  Copyright © 2016年 JustinYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface waveAnimationView : UIView
+
+@end
